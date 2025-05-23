bcf3b8cd5756c28ceb71d66599ad46ba7935810f
ImJustAPassenger <ferrauto1020@gmail.com>
2025-05-23 12:31:29 +0200

    completed the app

--- src/components/Events/NewEventsSection.jsx
+++ src/components/Events/NewEventsSection.jsx
@@ -1,16 +1,17 @@
-import {useQuery} from '@tanstack/react-query'
-import LoadingIndicator from '../UI/LoadingIndicator.jsx';
-import ErrorBlock from '../UI/ErrorBlock.jsx';
-import EventItem from './EventItem.jsx';
-import { fetchEvents } from '../../util/http.js';
+import { useQuery } from "@tanstack/react-query";
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+import EventItem from "./EventItem.jsx";
+import { fetchEvents } from "../../util/http.js";
 
 export default function NewEventsSection() {
-
-const {data,isPending, isError,error} =useQuery({
-  queryKey:['events'],
-  queryFn: fetchEvents,
-  staleTime:5000
-})
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["events", { max: 3 }],
+    queryFn: ({ signal, queryKey }) => {
+      fetchEvents({ signal, ...queryKey[1] });
+    },
+    staleTime: 5000,
+  });
 
   let content;
 
@@ -20,7 +21,10 @@ const {data,isPending, isError,error} =useQuery({
 
   if (isError) {
     content = (
-      <ErrorBlock title="An error occurred" message={error.info?.message||'Failed to fetch events'} />
+      <ErrorBlock
+        title="An error occurred"
+        message={error.info?.message || "Failed to fetch events"}
+      />
     );
   }
 
